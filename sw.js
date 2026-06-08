694a59513fb57ef23873daca3d39d4805a8e45dd
ilMacco1993 <dacchioli.matteo@icloud.com>
2026-06-08 17:55:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
-self.addEventListener("install", event => {
-  console.log("Service Worker installato");
+const CACHE = "ortodio-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./styles.css",
+        "./app.js",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
